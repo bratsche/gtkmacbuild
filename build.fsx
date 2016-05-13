ff7b10e9162e9cf27b2c505e3d5bbd13151167cf
Cody Russell <cody@jhu.edu>
2016-05-13 14:20:59 -0500

    Reorganization, add support for git projects, add gtk

--- build.fsx
+++ build.fsx
@@ -12,28 +12,73 @@ open Fake.StringHelper
 open Fake.Git
 open Fake.EnvironmentHelper
 
-let originDir = FileSystemHelper.currentDirectory
+// Package type definitions
+let majorVersion version =
+  let test = Regex.Match(version, "^[0-9]+\.[0-9]+")
+  match test.Success with
+  | true -> test.Groups.[0].Value
+  | false -> ""
 
-type PackageInfo = {
-  Url: string;
+
+type Version = Version of string
+type Url = Url of string
+type Branch = Branch of string
+type Revision = Revision of string
+
+type SourceType =
+  | Gnu
+  | Gnome
+  | GnomeXz
+  | FreeDesktop
+  | SourceForge
+  | Cairo
+  | CairoXz
+
+type PkgSource =
+  | Git of Url * Branch * Revision
+  | Tarball of Url * Version * string
+
+type SetupMethod =
+  | Configure
+  | Autogen
+
+type Pkg = {
   Name: string;
-  Version: string;
+  Source: PkgSource;
+  Setup: SetupMethod;
   ConfigFlags: string option;
 }
 
-type Arch =
-  X86 | X64 | Universal
+let defSource (srcType: SourceType) name version =
+  let url = match srcType with
+            | Gnu -> sprintf "ftp://ftp.gnu.org/gnu/%s/%s-%s.tar.gz" name name version
+            | Gnome -> sprintf "http://ftp.gnome.org/gnome/sources/%s/%s/%s-%s.tar.bz2" name (majorVersion(version)) name version
+            | GnomeXz -> sprintf "http://ftp.gnome.org/pub/gnome/sources/%s/%s/%s-%s.tar.xz" name (majorVersion(version)) name version
+            | FreeDesktop -> sprintf "http://%s.freedesktop.org/releases/%s-%s.tar.gz" name name version
+            | SourceForge -> sprintf "http://downloads.sourceforge.net/sourceforge/%s/%s-%s.tar.bz2" name name version
+            | Cairo -> sprintf "http://cairographics.org/releases/%s-%s.tar.gz" name version
+            | CairoXz -> sprintf "http://cairographics.org/releases/%s-%s.tar.xz" name version
+  Tarball (Url(url), Version(version), name)
 
-type OS =
-  Mac | Windows
+let gitSource url branch revno =
+  Git (Url (url), Branch (branch), Revision (revno))
 
-  static member Current =
-    match Environment.OSVersion.Platform with
-    | PlatformID.Unix -> Mac
-    | _ -> Windows
+let defCustomSource url name version =
+  Tarball (Url(url), Version(version), name)
+
+let forPackage name source =
+  { Name = name; Source = source; ConfigFlags = None; Setup = Configure }
+
+let configurePackage setup pkg =
+  {pkg with Setup = setup}
+
+let withConfigFlags flags pkg =
+  {pkg with ConfigFlags = Some(flags)}
+
 
 // Some directories
 // ------------------------------------------------------
+let originDir = FileSystemHelper.currentDirectory
 let systemMonoDir = "/Library/Frameworks/Mono.framework/Versions/Current"
 let installDir () = Path.Combine(originDir, "install")
 let buildDir () = Path.Combine(originDir, "root")
@@ -45,11 +90,6 @@ let srcDir () = Path.Combine(originDir, "src")
 // ------------------------------------------------------
 let universalLdFlags () = ["-arch i386"; "-arch x86_64"]
 
-let majorVersion version =
-  let test = Regex.Match(version, "^[0-9]+\.[0-9]+")
-  match test.Success with
-  | true -> test.Groups.[0].Value
-  | false -> ""
 
 let gnuUrl (name, version) = sprintf "ftp://ftp.gnu.org/gnu/%s/%s-%s.tar.gz" name name version
 let gnomeUrl (name, version) = sprintf "http://ftp.gnome.org/gnome/sources/%s/%s/%s-%s.tar.bz2" name (majorVersion(version)) name version
@@ -79,61 +119,80 @@ let filenameFromUrl (url: string) =
   |> List.rev
   |> List.head
 
-let download package =
-  let url = package.Url
-  let file = Path.Combine(srcDir(), Path.GetFileName url)
+let extract (filename, pkg) =
+  match pkg.Source with
+  | Tarball (_,_,_) -> sprintf ("-C %s -xf %s") (buildDir()) (filename) |> sh "tar"
+  | _ -> ()
 
-  trace(sprintf "Downloading file %s" url)
-
-  if not (File.Exists (file)) then
-    use client = new WebClient() in
-      client.DownloadFile(url, file)
-
-  (file, package)
-
-let extract (filename, package) =
-  trace(sprintf ("extract %s") (filename))
-  sprintf ("-C %s -xf %s") (buildDir()) (filename) |> sh "tar"
-
-  (filename, package)
+  (filename, pkg)
 
 let arch = sh "uname" "-m"
 
-let configure package =
-  trace("configure")
+let extractDirectory pkg =
+  match pkg.Source with
+  | Git (Url(url), branch, rev) ->
+    let regex = Regex.Match(url, "([A-Za-z0-9]+)\.git$")
+    match regex.Success with
+    | true ->
+      let s = regex.Groups.[0].Value
+      s.[0..(String.length(s) - 5)] // Remove the ".git" from the end
+    | false -> failwith "Invalid git URL"
+  | Tarball (url, Version(version), name) -> sprintf "%s-%s" name version
 
-  let configFlags = match package.ConfigFlags with
+let download (pkg) =
+  match pkg.Source with
+  | Git ( Url(url), Branch(branch), Revision(rev) ) ->
+    let checkoutDir = Path.Combine(buildDir(), extractDirectory(pkg))
+    if not (Directory.Exists(checkoutDir)) then
+      Git.Repository.cloneSingleBranch (buildDir()) url branch checkoutDir
+      Git.Reset.hard checkoutDir rev |> ignore
+
+    (checkoutDir, pkg)
+  | Tarball ( Url(url), version, name) ->
+    let file = Path.Combine(srcDir(), Path.GetFileName url)
+
+    trace(sprintf "Downloading file %s" url)
+
+    if not (File.Exists (file)) then
+      use client = new WebClient() in
+        client.DownloadFile(url, file)
+
+    (file, pkg)
+
+let configure pkg =
+  let configFlags = match pkg.ConfigFlags with
                     | None -> sprintf ("--prefix=%s") (installDir())
                     | Some flags -> sprintf ("%s --prefix=%s") (flags) (installDir())
+  let command = match pkg.Setup with
+                | Configure -> "configure"
+                | Autogen -> "autogen.sh"
 
-  let packageName = sprintf "%s-%s" package.Name package.Version
+  let packageName = extractDirectory pkg
   Path.Combine(buildDir(), packageName)
   |> from (fun () ->
-    sh "configure" configFlags
+    sh command configFlags
     |> ignore
   )
 
-  package
+  pkg
 
-let make package =
-  trace("make")
-
-  let packageName = sprintf "%s-%s" package.Name package.Version
+let make pkg =
+  let packageName = extractDirectory pkg
   Path.Combine(buildDir(), packageName)
   |> from (fun () ->
     sh "make" "-j 4" |> ignore
   )
 
-  package
+  pkg
 
-let install package =
-  let packageName = sprintf "%s-%s" package.Name package.Version
+let install pkg =
+  let packageName = extractDirectory pkg
   Path.Combine(buildDir(), packageName)
   |> from (fun () ->
     sh "make" "install" |> ignore
   )
 
-  package
+  pkg
 
 let withEnvironment (name: string) (value: string) (action: unit -> unit) =
   let oldEnv = environVarOrNone name
@@ -145,16 +204,16 @@ let withEnvironment (name: string) (value: string) (action: unit -> unit) =
   | None    -> ()
   | Some(x) -> setProcessEnvironVar name x
 
-let build (filename, package) =
+let build (filename, pkg) =
   ensureDirectory (installDir())
 
-  package
+  pkg
   |> configure
   |> make
   |> install
 
-let startBuild package =
-  EnvironmentHelper.setEnvironVar "PATH" (sprintf ("%s/bin:/usr/bin:/bin:/usr/local/git/bin") (installDir()))
+let startBuild pkg =
+  EnvironmentHelper.setEnvironVar "PATH" (sprintf ("%s/bin:/usr/bin:/bin:/usr/local/bin") (installDir()))
   EnvironmentHelper.setEnvironVar "CFLAGS" (sprintf ("-I%s/include") (installDir()))
   EnvironmentHelper.setEnvironVar "LD_LIBRARY_PATH" (sprintf ("%s/lib") (installDir()))
   EnvironmentHelper.setEnvironVar "LDFLAGS" (sprintf ("-L%s/lib") (installDir()))
@@ -164,8 +223,9 @@ let startBuild package =
   EnvironmentHelper.setEnvironVar "XDG_CONFIG_DIRS" (sprintf ("%s/etc/xdg") (installDir()))
   EnvironmentHelper.setEnvironVar "XDG_CONFIG_HOME" "$HOME/.config"
   EnvironmentHelper.setEnvironVar "XDG_DATA_DIRS" (sprintf ("%s/share") (installDir()))
+  EnvironmentHelper.setEnvironVar "JHBUILD_PREFIX" (installDir())
 
-  package
+  pkg
   |> download
   |> extract
   |> build
@@ -177,102 +237,129 @@ Target "prep" <| fun _ ->
   trace("prep")
 
 Target "autoconf" <| fun _ ->
-  let version = "2.69"
-  { Url = gnuUrl("autoconf", version); Name = "autoconf"; Version = version; ConfigFlags = None }
+  defSource Gnu "autoconf" "2.69"
+  |> forPackage "autoconf"
   |> startBuild
 
 Target "automake" <| fun _ ->
-  let version = "1.13.4"
-  { Url = gnuUrl("automake", version); Name = "automake"; Version = version; ConfigFlags = None }
+  defSource Gnu "automake" "1.13.4"
+  |> forPackage "autoconf"
   |> startBuild
 
 Target "libtool" <| fun _ ->
-  let version = "2.4.2"
-  { Url = gnuUrl("libtool", version); Name = "libtool"; Version = version; ConfigFlags = None }
+  defSource Gnu "libtool" "2.4.2"
+  |> forPackage "autoconf"
   |> startBuild
 
 Target "pkgconfig" <| fun _ ->
-  let version = "0.27"
-  { Url = fdoUrl("pkg-config", version); Name = "pkg-config"; Version = version; ConfigFlags = Some("--with-internal-glib") }
+  defSource FreeDesktop "pkg-config" "0.27"
+  |> forPackage "pkg-config"
+  |> withConfigFlags "--with-internal-glib"
   |> startBuild
 
 Target "gettext" <| fun _ ->
-  let version = "0.18.2"
-  { Url = gnuUrl("gettext", version); Name = "gettext"; Version = version; ConfigFlags = None }
+  defSource Gnu "gettext" "0.18.2"
+  |> forPackage "gettext"
   |> startBuild
 
 Target "freetype" <| fun _ ->
-  let version = "2.5.0.1"
   ensureDirectory (Path.Combine(installDir(), "include", "freetype2", "freetype", "cache"))
   ensureDirectory (Path.Combine(installDir(), "include", "freetype2", "freetype", "internal"))
-  { Url = sfUrl("freetype", version); Name = "freetype"; Version = version; ConfigFlags = None }
+
+  defSource SourceForge "freetype" "2.5.0.1"
+  |> forPackage "freetype"
   |> startBuild
 
 Target "libffi" <| fun _ ->
   let version = "3.0.13"
   let url = sprintf "ftp://sourceware.org/pub/libffi/libffi-%s.tar.gz" version
-  { Url = url; Name = "libffi"; Version = version; ConfigFlags = Some("--srcdir=/Users/cody/xam/gtkmacbuild/root/libffi-3.0.13") }
+
+  defCustomSource url "libffi" version
+  |> forPackage "libffi"
+  |> withConfigFlags "--srcdir=/Users/cody/xam/gtkmacbuild/root/libffi-3.0.13" // TODO
   |> startBuild
 
 Target "glib" <| fun _ ->
-  let version = "2.36.4"
-  { Url = gnomeUrlXz("glib", version); Name = "glib"; Version = version; ConfigFlags = Some("--disable-compile-warnings") }
+  defSource GnomeXz "glib" "2.36.4"
+  |> forPackage "glib"
+  |> withConfigFlags "--disable-compile-warnings"
   |> startBuild
 
 Target "harfbuzz" <| fun _ ->
   trace("harfbuzz")
 
 Target "atk" <| fun _ ->
-  let version = "2.8.0"
-  { Url = gnomeUrlXz("atk", version); Name = "atk"; Version = version; ConfigFlags = None }
+  defSource GnomeXz "atk" "2.8.0"
+  |> forPackage "atk"
   |> startBuild
 
 Target "gdk-pixbuf" <| fun _ ->
-  let version = "2.28.2"
-  { Url = gnomeUrlXz("gdk-pixbuf", version); Name = "gdk-pixbuf"; Version = version; ConfigFlags = None }
+  defSource GnomeXz "gdk-pixbuf" "2.28.2"
+  |> forPackage "gdk-pixbuf"
   |> startBuild
 
 Target "fontconfig" <| fun _ ->
   let version = "2.10.2"
   let url = sprintf "http://www.fontconfig.org/release/fontconfig-%s.tar.gz" version
-  { Url = url; Name = "fontconfig"; Version = version; ConfigFlags = Some("--disable-docs") }
+
+  defCustomSource url "fontconfig" version
+  |> forPackage "fontconfig"
+  |> withConfigFlags "--disable-docs"
   |> startBuild
 
-
 Target "pixman" <| fun _ ->
-  let version = "0.30.0"
-  { Url = cairoUrl("pixman", version); Name = "pixman"; Version = version; ConfigFlags = None }
+  defSource Cairo "pixman" "0.30.0"
+  |> forPackage "pixman"
   |> startBuild
 
 Target "cairo" <| fun _ ->
-  let version = "1.12.14"
-  { Url = cairoUrlXz("cairo", version); Name = "cairo"; Version = version; ConfigFlags = Some("--enable-pdf --enable-quartz --enable-quartz-font --enable-quartz-image --disable-xlib --without-x") }
+  defSource CairoXz "cairo" "1.12.14"
+  |> forPackage "cairo"
+  |> withConfigFlags "--enable-pdf --enable-quartz --enable-quartz-font --enable-quartz-image --disable-xlib --without-x"
   |> startBuild
 
 Target "pango" <| fun _ ->
-  let version = "1.35.0"
-  { Url = gnomeUrlXz("pango", version); Name = "pango"; Version = version; ConfigFlags = Some("--without-x") }
+  defSource GnomeXz "pango" "1.35.0"
+  |> forPackage "pango"
+  |> withConfigFlags "--without-x"
+  |> startBuild
+
+Target "gtk-osx-docbook" <| fun _ ->
+  gitSource "git@github.com:jralls/gtk-osx-docbook.git" "master" "058d8a2f3f0d37de00b8e9ac78f633706deb5e22"
+  |> forPackage "docbook"
+  |> startBuild
+
+Target "gtk-doc" <| fun _ ->
+  defSource GnomeXz "gtk-doc" "1.18"
+  |> forPackage "gtk-doc"
   |> startBuild
 
 Target "gtk" <| fun _ ->
-  trace("gtk")
+  gitSource "git@github.com:bratsche/gtk.git" "xamarin-mac" "HEAD"
+  |> forPackage "gtk"
+  |> configurePackage Autogen
+  |> withConfigFlags "--with-gdktarget=quartz --disable-man"
+  |> startBuild
 
 Target "xamarin-gtk-theme" <| fun _ ->
   trace("xamarin-gtk-theme")
 
 Target "libpng" <| fun _ ->
-  let version = "1.4.12"
-  { Url = sfUrl("libpng", version); Name = "libpng"; Version = version; ConfigFlags = None }
+  defSource SourceForge "libpng" "1.4.12"
+  |> forPackage "libpng"
   |> startBuild
 
 Target "libjpeg" <| fun _ ->
-  { Url = "http://www.ijg.org/files/jpegsrc.v8.tar.gz"; Name = "jpeg"; Version = "8"; ConfigFlags = None}
+  defCustomSource "http://www.ijg.org/files/jpegsrc.v8.tar.gz" "jpeg" "8"
+  |> forPackage "libjpeg"
   |> startBuild
 
 Target "libtiff" <| fun _ ->
   let version = "4.0.3"
   let url = sprintf "http://download.osgeo.org/libtiff/tiff-%s.tar.gz" version
-  { Url = url; Name = "tiff"; Version = version; ConfigFlags = None}
+
+  defCustomSource url "tiff" version
+  |> forPackage "tiff"
   |> startBuild
 
 Target "libgif" <| fun _ ->
